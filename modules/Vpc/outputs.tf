666ca0b50971aa5ae9a596006ca9601635da1a27
Ayush_Saini <73630171+Ayushsaini20@users.noreply.github.com>
2024-11-13 13:15:40 +0530

    Delete modules/Vpc/outputs.tf

--- modules/Vpc/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = aws_vpc.this.id
-}
-
-output "subnet_ids" {
-  description = "List of subnet IDs"
-  value       = aws_subnet.subnet[*].id
-}
